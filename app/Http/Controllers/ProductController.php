338e3b4a76024b7d7143ccb3ef6dfc04d3e85a9f
Nico Di Marco <ndimarco@avantio.com>
2021-04-25 21:29:41 +0200

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\ProductCreated;
+use App\Jobs\ProductDeleted;
+use App\Jobs\ProductUpdated;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = Product::create($request->only('title', 'image'));
+
+        ProductCreated::dispatch($product->toArray())->onQueue('main_queue');
+
+        return response($product, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        $product->update($request->only('title', 'image'));
+
+        ProductUpdated::dispatch($product->toArray())->onQueue('main_queue');
+
+        return response($product, Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Product::destroy($id);
+
+        ProductDeleted::dispatch($id)->onQueue('main_queue');
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use App\Jobs\ProductLiked;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Event;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        App::bindMethod(ProductLiked::class . '@handle', function ($job) {
+            return $job->handle();
+        });
+    }
+}
